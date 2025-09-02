01874846fc02e97d68223a13eac7f5b703249d16
GuilhermeGimenesTrue <guilherme.gimenes260609@gmail.com>
2025-09-02 08:45:22 -0300

    Repository

--- Aula14/loja/form_atualiza.php
+++ Aula14/loja/form_atualiza.php
@@ -1,28 +1,43 @@
 <?php 
     include 'pedaco.php';
+    $id = $_GET['id'];
 ?>
 
 
 
 
     <div class="container">
-        <form action="atualizar.php" method="POST">
+        <form action="atualizar.php?id= <?php echo $id ?> "  method="POST">
 
             <?php
-                $id = $_GET['id'];
+                
                 echo "Valor passado: $id";
+
+                require 'conexao.php';
+                $sql = "SELECT * FROM produtos WHERE id = $id";
+                $stmt = $pdo->query($sql);
+                $produto = $stmt->fetch(PDO::FETCH_ASSOC);
+
+                print_r($produto);
+
+             /*    while ($produto = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                        echo "ID: " . $produto['id'] . "<br>";
+                        echo "Nome: " . $produto['nome'] . "<br>";
+                        echo "Preço: R$" . $produto['preco'] . "<br>";
+                        echo "Estoque: " . $produto['estoque'] . "<br><br>";
+                    } */
             ?>
 
 
              
             <div class="mb-3">
-                Nome: <input type="text" class="form-control" name="nome">
+                Nome: <input type="text" value="<?php echo $produto['nome']; ?>" class="form-control" name="nome_novo">
             </div>
             <div class="mb-3">
-                Preco: <input type="text" class="form-control" name="preco">
+                Preco: <input type="text" value="<?php echo $produto['preco']; ?>" class="form-control" name="preco_novo">
             </div>
             <div class="mb-3">
-                Quantidade: <input type="text" class="form-control" name="quantidade">
+                Quantidade: <input type="text" value="<?php echo $produto['quantidade']; ?>"class="form-control" name="quantidade_novo">
             </div>
         
             <button type="submit" class="btn btn-primary">Atualizar</button>
